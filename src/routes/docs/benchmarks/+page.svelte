e5f13232899a2eae74746f8386077ae36eac8970
Ryan Atkinson <mail@ryanatkn.com>
2026-07-16 11:22:22 -0400

    wip

--- src/routes/docs/benchmarks/+page.svelte
+++ src/routes/docs/benchmarks/+page.svelte
@@ -84,7 +84,7 @@
 		</ul>
 		<p>
 			Each section below has notes that attempt to fairly contextualize its numbers. The charts show
-			numbers using Node, and at the end of the page is a <a href="#Cross-runtime"
+			numbers using Node v24, and at the end of the page is a <a href="#Cross-runtime"
 				>cross-runtime comparison</a
 			>.
 		</p>
